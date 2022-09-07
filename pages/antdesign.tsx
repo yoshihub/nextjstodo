a0bb557e49f8b9c669abc036447f7b544b1f1fe8
yoshihub <saiga123.nm@gmail.com>
2022-09-08 05:15:46 +0900

    アントデザインのbutton

--- pages/antdesign.tsx
+++ pages/antdesign.tsx
@@ -1,11 +1,20 @@
 import { Button } from 'antd';
+import {useState} from 'react';
 
 const AntDesign: React.FC= () => {
+  const [loading,setLoading]=useState(false)
 
+  const onButtonClick=()=>{
+    console.log("クリック")
+    setLoading(true)
+    setTimeout(()=>{
+       setLoading(false)
+    },2000);
+  }
 
 
   return (
-    <Button>Default Button</Button>
+    <Button type="primary" onClick={onButtonClick}>First Button</Button>
   )
 
 }
